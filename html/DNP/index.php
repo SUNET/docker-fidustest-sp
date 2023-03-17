0fff10689ec923702d3221845709e5d441b2e788
Björn Mattsson <bjorn@sunet.se>
2023-03-17 09:05:24 +0100

    Updated DNP to digitala nationella prov

--- html/DNP/index.php
+++ html/DNP/index.php
@@ -1,8 +1,9 @@
 <?php
 include '../assets/html.php';
 print_header('Skolverkets testsida för inloggning','../');?>
-        <h2>Test av inloggningstjänst mot DNP</h2>
+        <h2>Test av inloggningstjänst mot digitala nationella prov</h2>
         <p>Från denna sida kan du testa följande:
-	<p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/secure&entityID=https://idpproxy.dev.eduid.se/idp"><button class="button">Utan MFA</button></a></p>
-	<p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/refeds_mfa&entityID=https://idpproxy.dev.eduid.se/idp&authnContextClassRef=https://refeds.org/profile/mfa"><button class="button">Med MFA</button></a></p>
+        <p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/secure&entityID=https://idpproxy.dev.eduid.se/idp"><button class="button">Inloggning utan e-legitimation</button></a></p>
+        <!--p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/refeds_mfa&entityID=https://idpproxy.dev.eduid.se/idp&authnContextClassRef=https://refeds.org/profile/mfa"><button class="button">Med MFA</button></a></p-->
+        <p>För att kunna använda testet behöver ni en inloggningstjänst (IdP) ansluten till en identitetsfederation i FIDUS.</p>
 <?php print_footer();
--- html/index.php
+++ html/index.php
@@ -4,6 +4,6 @@ print_header('Skolverkets testsida för inloggning');?>
         <h2>Test av inloggningstjänst</h2>
         <p>Från denna sida kan du testa följande:
 	<p><a href="login"><button class="button">Test av inloggningstjänst</button></a></p>	
-	<p><a href="DNP"><button class="button">Test inför DNP</button></a></p>
+	<p><a href="DNP"><button class="button">Test inför digitala nationella prov</button></a></p>
 	<p>Du når testsidan via en inloggning på din skola, även kallad federerad inloggning. Skolverket hanterar inte dina inloggningsuppgifter, och inga uppgifter sparas av Skolverket.</p>
 <?php print_footer();
