4c2bf2a795b3c6310bec1af2e08d2ebc864f31fe
Björn Mattsson <bjorn@sunet.se>
2023-08-15 10:24:51 +0200

    Updated link based on mail from Mattias Hyll

--- html/DNP-staging/index.php
+++ html/DNP-staging/index.php
@@ -9,7 +9,7 @@ print_header('Skolverkets testsida för inloggning','../');?>
 <?php print_footer('', '
 	<p><a href="https://www.skolverket.se/om-oss/var-verksamhet/skolverkets-prioriterade-omraden/digitalisering/digitala-nationella-prov/tekniska-forutsattningar-for-digitala-nationella-prov/vagledning-och-tekniska-verifieringstester">Information om Skolverkets tekniska verifieringstester</a></p>
 	<p><a href="https://www.skolverket.se/om-oss/var-verksamhet/skolverkets-prioriterade-omraden/digitalisering/digitala-nationella-prov/tekniska-forutsattningar-for-digitala-nationella-prov/interfederationen-fidus">Interfederationen Fidus</a></p>
-	<p><a href="https://www.skolverket.se/om-oss/var-verksamhet/skolverkets-prioriterade-omraden/digitalisering/digitala-nationella-prov/tekniska-forutsattningar-for-skolorna-att-kunna-genomfora-digitala-nationella-prov/eppn">Information om federationsanvändarnamn (EPPN)</a></p>
+	<p><a href="https://www.skolverket.se/om-oss/var-verksamhet/skolverkets-prioriterade-omraden/digitalisering/digitala-nationella-prov/tekniska-forutsattningar-for-digitala-nationella-prov/eppn">Information om federationsanvändarnamn (EPPN)</a></p>
 	<p><a href="https://www.skolverket.se/om-oss/var-verksamhet/skolverkets-prioriterade-omraden/digitalisering/digitala-nationella-prov/tekniska-forutsattningar-for-skolorna-att-kunna-genomfora-digitala-nationella-prov">Tekniska förutsättningar för att genomföra digitala nationella prov</a></p>
 	<p><a href="https://www.skolverket.se/om-oss/var-verksamhet/skolverkets-prioriterade-omraden/digitalisering/digitala-nationella-prov/nu-blir-nationella-prov-digitala">Nu blir nationella prov digitala </a></p>
 ');
