6a7ad2dd927ad5aa7a905a864f05db2e2779144a
Björn Mattsson <bjorn@sunet.se>
2023-09-15 14:00:57 +0200

    Changed to prod-proxy in /DNP

--- html/DNP/index.php
+++ html/DNP/index.php
@@ -3,8 +3,8 @@ include '../assets/html.php';
 print_header('Skolverkets testsida för inloggning','../');?>
         <h2>Test av inloggningstjänst mot digitala nationella prov</h2>
         <p>Från denna sida kan du testa följande:
-        <p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/secure&entityID=https://idpproxy.dev.eduid.se/idp"><button class="button">Inloggning utan e-legitimation</button></a></p>
-        <!--p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/refeds_mfa&entityID=https://idpproxy.dev.eduid.se/idp&authnContextClassRef=https://refeds.org/profile/mfa"><button class="button">Med MFA</button></a></p-->
+        <p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/secure/DNP.php&entityID=https://skolverket.eduid.se/dnp/idp/"><button class="button">Inloggning utan e-legitimation</button></a></p>
+        <p><a href="/Shibboleth.sso/Login?target=https://fidustest.skolverket.se/refeds_mfa/DNP.php&entityID=https://skolverket.eduid.se/dnp/idp/&authnContextClassRef=https://refeds.org/profile/mfa"><button class="button">Inloggning med e-legitimation</button></a></p>
 	<p>För att kunna använda testet behöver ni en inloggningstjänst (IdP) ansluten till en identitetsfederation i FIDUS.</p>
 <?php print_footer('', '
 	<p><a href="https://www.skolverket.se/om-oss/var-verksamhet/skolverkets-prioriterade-omraden/digitalisering/digitala-nationella-prov/tekniska-forutsattningar-for-digitala-nationella-prov/vagledning-och-tekniska-verifieringstester">Information om Skolverkets tekniska verifieringstester</a></p>
